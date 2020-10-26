59e4b5a4b77baed10c3011617e9294b9d47c8660
Arjun Nair <arjun@nairsnet.com>
2020-10-26 17:13:38 -0400

    setup working express server

---
+++ README.md
@@ -0,0 +1,11 @@
+Setup code for Capstone project by Arjun, Elias, Anthony, and David
+
+client is written in React and is stored "client"
+
+server is written in Express" and is stored in "server"
+
+To begin, cd into both directories and run "npm install"
+
+to run client, cd into client and run "npm run start:dev" and navigate to https://localhost:3000
+
+to run server, cd into server and run "npm run start:dev". server runs on https://localhost:3001
---
+++ server/src/routes/index.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+// import UserRouter from './Users';
+import testAPI from './testApi'
+
+// Init router and path
+const router = Router();
+
+// router = testAPI;
+// Add sub-routes
+// router.use('/users', UserRouter);
+router.use("/testAPI", testAPI);
+
+// router.use("/", (req, res) => {
+//     res.json({"we": "here now"});
+// })
+
+
+router.use("/test", (req, res) => {
+    res.json({
+        "this": "is a test"
+    });
+})
+
+// Export the base-router
+export default router;
